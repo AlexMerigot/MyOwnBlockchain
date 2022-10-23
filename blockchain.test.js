19eda7c1a2dec313fcf3576a9272f3aa86d59a67
Major <104435310+AlexMerigot@users.noreply.github.com>
2022-10-23 13:39:30 +0200

    Update blockchain.test.js

--- blockchain.test.js
+++ blockchain.test.js
@@ -26,45 +26,45 @@ describe('Blockchain', () => {
         expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
     });
 
-    // describe('isValidChain()', () => {
-    //     describe('When the chain does not start with the genesis block', () => {
-    //         it('return false', () => {
-    //             blockchain.chain[0] = {data:'fake-genesis'};
+    describe('isValidChain()', () => {
+        describe('When the chain does not start with the genesis block', () => {
+            it('return false', () => {
+                blockchain.chain[0] = {data:'fake-genesis'};
 
-    //             expect(Blockchain.isValidChain(Blockchain.chain)).toBe(false);
-    //         });
-    //     });
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
 
-    //     describe('when the chain starts with the genesis block and has multiple blocks', () => {
-    //         beforeEach(() => {
-    //             blockchain.addBlock({data: 'Bears'});
-    //             blockchain.addBlock({data: 'Beets'});
-    //             blockchain.addBlock({data: 'Battlestar Galactica'});
-    //         });
+        describe('when the chain starts with the genesis block and has multiple blocks', () => {
+            beforeEach(() => {
+                blockchain.addBlock({data: 'Bears'});
+                blockchain.addBlock({data: 'Beets'});
+                blockchain.addBlock({data: 'Battlestar Galactica'});
+            });
 
-    //         describe('and a lastHash reference has changed', () => {
-    //             it('return false', () => {
-    //                 blockchain.chain[2].lastHash = 'broken-lastHash';
+            describe('and a lastHash reference has changed', () => {
+                it('return false', () => {
+                    blockchain.chain[2].lastHash = 'broken-lastHash';
 
-    //                 expect(Blockchain.isValidChain(Blockchain.chain)).toBe(false);
-    //             });
-    //         });
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
 
-    //         describe('and the chain contains a block with an invalid field ', () => {
-    //             it('return false', () => {
-    //                 blockchain.chain[2].data = 'some-bad-and-evil-data';
+            describe('and the chain contains a block with an invalid field ', () => {
+                it('return false', () => {
+                    blockchain.chain[2].data = 'some-bad-and-evil-data';
 
-    //                 expect(Blockchain.isValidChain(Blockchain.chain)).toBe(false);
-    //             });
-    //         });
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
 
-    //         describe('and the chain does not contain any invalid blocks', () => {
-    //             it('return true', () => {
-    //                 expect(Blockchain.isValidChain(Blockchain.chain)).toBe(true);
-    //             });
-    //         });
-    //     });
-    // });
+            describe('and the chain does not contain any invalid blocks', () => {
+                it('return true', () => {
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
+    });
 
     describe('replaceChain()', () => {
         let errorMock, logMock;
